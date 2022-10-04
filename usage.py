913684b431a38040a1220f9a62f24f413442a3ca
Aavtic <89965681+Aavtic@users.noreply.github.com>
2022-10-04 08:18:18 -0700

    Delete usage.py

--- usage.py
+++
@@ -1,8 +0,0 @@
-import MainApi
-
-cookies = {}
-headers = {}
-main = MainApi.InstaVideoDownloader(cookies=cookies, headers=headers)
-link =  'THE INSTAGRAM VIDEO LINK'
-link = main.get_info(link)
-main.get_raw(link)
